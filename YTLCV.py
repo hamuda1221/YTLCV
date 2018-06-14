fbe717b16ac4ca01487b4d360b182a8f4c873594
hamuda <bonresuhamuda@gmail.com>
2018-06-15 00:09:11 +0900

    URL入力とコメント表示まで実装

--- YTLCV.py
+++ YTLCV.py
@@ -17,8 +17,9 @@ from oauth2client import tools
 from oauth2client import client
 from oauth2client.file import Storage
 
-pageToken, http, url, ffffff, chat_no = None, None, None, 0, 0
+pageToken, http, url, ffffff, chat_no, rrrr = None, None, None, 0, 0, 0
 
+"""
 def pushButton(event):
     #ボタンが押されたらコメントを読み取り始める
     credentials_path = "credentials.json"
@@ -72,12 +73,13 @@ def roop():
     data = json.loads(data.decode())
 
     for datum in data["items"]:
-        """
+
         if datum["snippet"]["superChatDetails"] == True:
             text1_1.insert(str(chat_no + 1) + ".0", str(chat_no) + "\n")
             text1_3.insert(str(chat_no + 1) + ".0", str(datum["snippet"]["displayMessage"]) + "\n")
             #datum["authorDetails"]["profileImageUrl"])
-        """
+
+
         try:
             if datum["authorDetails"]["isChatModerator"] == True:
                 print("==========================================================")
@@ -96,7 +98,7 @@ def roop():
     pageToken = data["nextPageToken"]
     #time.sleep(3)
     root.after(5000, roop)
-
+    """
 
 #ウィンドの作成
 class MyApp(wx.Frame):
@@ -107,39 +109,141 @@ class MyApp(wx.Frame):
         self.init_ui()
 
     def init_ui(self):
+        
         self.SetTitle('test')
-        self.SetBackgroundColour((21, 31, 42))
-        self.SetPosition((1450, 530))
-        self.SetSize((420, 320))
+        #self.SetBackgroundColour((21, 31, 42))
+        self.SetPosition((1080, 35))
+        self.SetSize((800, 1000))
         
-        panel_URL = wx.Panel(self, -1, size = (380, 40), pos = (10, 10))
-        panel_URL.SetBackgroundColour((41, 62, 84))
-        self.label_URL = wx.StaticText(panel_URL, -1, "動画URL", size = (50, 35), pos = (0, 5))
-        self.box = wx.TextCtrl(panel_URL, -1, size = (245, 30), pos = (55,5))
+        self.panel = wx.Panel(self, -1)
+        self.panel_chat = wx.BoxSizer(wx.VERTICAL)
+        self.panel_ui1 = wx.BoxSizer(wx.HORIZONTAL)
+        
+        self.label_URL = wx.StaticText(self.panel, -1, "動画URL")#, size = (50, 35), pos = (0, 5))
+        self.box = wx.TextCtrl(self.panel, -1, size = (300, 20))#, pos = (55,5))
+        
 
-        btn_URL = wx.Button(panel_URL, -1, "接続", size = (70, 30), pos = (305, 5))
+        btn_URL = wx.Button(self.panel, -1, "接続")#, size = (70, 30), pos = (305, 5))
         btn_URL.Bind(wx.EVT_BUTTON, self.clicked)
-        
      
-        panel_R = wx.Panel(self, -1, pos = (10, 60), size = (180, 210))
-        panel_R.SetBackgroundColour((255, 0, 0))
+        #self.panel_R = wx.Panel(self, -1, size = (380, 400), pos = (10, 60))
+        #self.panel_R.SetBackgroundColour((0, 255, 0))
         
-        panel_ui1 = wx.Panel(panel_R, -1, pos = (5, 5), size=(175, 205))
-        self.label_jp = wx.StaticText(panel_ui1, -1, "てすとpppp", size = (170, 200))
+        #self.label_jp = wx.StaticText(panel_ui1, -1, "", size = (355, 200))
         
+        self.panel_chat.Add(self.panel_ui1)
+        self.panel_ui1.Add(self.label_URL)
+        self.panel_ui1.Add(self.box)
+        self.panel_ui1.Add(btn_URL)
+        
+        self.panel.SetSizer(self.panel_chat)
+        
+        """
         panel_B = wx.Panel(self, -1, pos = (210, 60), size = (180, 210))
         panel_B.SetBackgroundColour((0, 0, 255))
 
         panel_ui2 = wx.Panel(panel_B, -1, pos = (5, 5), size=(175, 205))
         self.label_en = wx.StaticText(panel_ui2, -1, "", size = (170, 200))
         self.label_en.SetLabel("aaaaaaaaaaaaaaaaaaaaaaaaa.")
+        """
+        
+        #コメント更新用
+        self.timer = wx.Timer(self)
+        self.Bind(wx.EVT_TIMER, self.roop)
+        self.timer.Start(5000)
 
-        self.Show()
+
+        self.Show(True)
         
+        #self.clicked()
         
+    def roop(self, event):
+        global url
+        global http
+        global pageToken
+        global ffffff
+        global chat_no
+        global rrrr
+
+        if rrrr != 0:
+    
+            if ffffff == 0:
+                url2 = url
+                ffffff = 1
+            else:
+                url2 = url + "&pageToken=" + pageToken
+        
+            res, data = http.request(url2)
+            f = open("data.json", "w")
+            json.dump(data.decode(), f)
+            data = json.loads(data.decode())
+            
+            
+            for datum in data["items"]:
+                """
+                if datum["snippet"]["superChatDetails"] == True:
+                    text1_1.insert(str(chat_no + 1) + ".0", str(chat_no) + "\n")
+                    text1_3.insert(str(chat_no + 1) + ".0", str(datum["snippet"]["displayMessage"]) + "\n")
+                    #datum["authorDetails"]["profileImageUrl"])
+                """
+                try:
+                    if datum["authorDetails"]["isChatModerator"] == True:
+                        print("==========================================================")
+
+                    text = (str("    ") + str(chat_no) + str(" | ") +\
+                            datum["authorDetails"]["displayName"] + str(" | ") +\
+                            datum["snippet"]["textMessageDetails"]["messageText"])
+                    #datum["authorDetails"]["profileImageUrl"])
+                    text = wx.StaticText(self.panel, -1, text)
+                    text.SetForegroundColour('#000000')
+                except KeyError:
+                    print("error")
+                chat_no += 1
+                self.panel_chat.Add(text, proportion=0)
+                self.panel_chat.Layout()
+                #self.label_jp.SetLabel(text)
+                print(chat_no)
+
+            pageToken = data["nextPageToken"]
+    
+    
+    
     def clicked(self, event):
-        text = self.box.GetValue()
-        self.label_jp.SetLabel(text)           
+        credentials_path = "credentials.json"
+        if os.path.exists(credentials_path):
+            # 認証済み
+            store = Storage(credentials_path)
+            credentials = store.get()
+        else:
+            # 認証処理
+            f = "client.json"
+            scope = "https://www.googleapis.com/auth/youtube.readonly"
+            flow = client.flow_from_clientsecrets(f, scope)
+            flow.user_agent = "hoge"
+            credentials = tools.run_flow(flow, Storage(credentials_path))
+            
+        global http
+        http = credentials.authorize(httplib2.Http())
+        
+        global url
+        
+        url = self.box.GetValue()
+        #url = "https://www.youtube.com/watch?v=XfE6h2xyumQ"
+        index1 = url.find('=')
+        url2 = url[index1 + 1 :]
+        url = "https://www.googleapis.com/youtube/v3/videos?part=liveStreamingDetails&id="
+        url += url2
+        res, data = http.request(url)
+        data = json.loads(data.decode())
+        chat_id = data["items"][0]["liveStreamingDetails"]["activeLiveChatId"]
+        print(chat_id)
+    
+        url = "https://www.googleapis.com/youtube/v3/liveChat/messages?part=snippet,authorDetails"
+        url += "&liveChatId=" + chat_id
+        
+        global rrrr
+        rrrr = 1
+        #self.label_jp.SetLabel(url)           
 
         
 app = wx.App()
@@ -203,6 +307,7 @@ root.mainloop()
 
 url = "https://www.googleapis.com/youtube/v3/videos?part=liveStreamingDetails&id="
 url += "Vy1RbiSvHoU"
+#https://www.youtube.com/watch?v=caunmjM4ez4
 res, data = http.request(url)
 data = json.loads(data.decode())
 chat_id = data["items"][0]["liveStreamingDetails"]["activeLiveChatId"]
